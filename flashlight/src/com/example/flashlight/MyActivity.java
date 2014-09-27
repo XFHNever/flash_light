9835fb8b91cb031e2776a16a54f05d44e4f924ac
XFHNever <443696948@qq.com>
2014-09-27 14:55:18 +0800

    add shortcut when installing

--- flashlight/src/com/example/flashlight/MyActivity.java
+++ flashlight/src/com/example/flashlight/MyActivity.java
@@ -1,10 +1,15 @@
 package com.example.flashlight;
 
 import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.database.Cursor;
 import android.hardware.Camera;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.os.Parcelable;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ImageView;
@@ -26,6 +31,10 @@ public class MyActivity extends Activity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         this.initView();
+
+        if (!isAddShortcut()) {
+             addShortcut();
+        }
     }
 
     /**
@@ -104,5 +113,61 @@ public class MyActivity extends Activity implements View.OnClickListener {
         return super.onKeyDown(keyCode, event);
     }
 
+    /**
+     * check if shortcut is installed
+     *
+     * @return
+     */
+    private boolean isAddShortcut() {
+        boolean isInstallShortcut = false;
 
+        final ContentResolver resolver = this.getContentResolver();
+
+        String AUTHORITY = "com.android.launcher.settings";
+        final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY
+                + "/favorites?notify=false");
+        Cursor cursor = resolver.query(CONTENT_URI, new String[]{"title", "iconResource"},
+                "title=?",new String[] { getString(R.string.app_name) }, null);
+
+        if (cursor != null && cursor.getCount() > 0) {
+            isInstallShortcut = true;
+        }
+
+        return isInstallShortcut;
+    }
+
+    /**
+     * install shortcut.
+     */
+    private void addShortcut() {
+        Intent shortcut = new Intent("com.android.launcher.action.INSTALL_SHORTCUT");
+        //set parameters
+        shortcut.putExtra(Intent.EXTRA_SHORTCUT_NAME, getResources().getString(R.string.app_name));
+
+        //set the icon of shortcut
+        Parcelable icon = Intent.ShortcutIconResource.fromContext(this,R.drawable.icon);
+        shortcut.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE,icon);
+
+        //if allows to add repeatedly
+        shortcut.putExtra("duplicate", false);
+
+        //set the operation when clicking the shortcut
+        Intent intent = new Intent(this, this.getClass());
+        intent.setAction(Intent.ACTION_MAIN);
+        intent.setFlags(Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
+        intent.addFlags(Intent.FLAG_ACTIVITY_LAUNCHED_FROM_HISTORY);
+        intent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+        shortcut.putExtra(Intent.EXTRA_SHORTCUT_INTENT, intent);
+
+        //broadcast
+        this.sendBroadcast(shortcut);
+    }
+
+    /**
+     * remove shortcut
+     */
+    private void removeShortcut() {
+
+    }
 }
